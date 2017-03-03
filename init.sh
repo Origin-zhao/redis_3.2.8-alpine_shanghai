86275a8980427bf31388b0c84f71e0dd33738c75
Origin-zhao <enginezy@gmail.com>
2017-03-03 17:05:28 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,19 @@
 echo 512 > /proc/sys/net/core/somaxconn
 echo "vm.overcommit_memory=1" > /etc/sysctl.conf
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
+
+set -e
+
+# first arg is `-f` or `--some-option`
+# or first arg is `something.conf`
+if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
+	set -- redis-server "$@"
+fi
+
+# allow the container to be started with `--user`
+if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
+	chown -R redis .
+	exec su-exec redis "$0" "$@"
+fi
+
+exec "$@"
