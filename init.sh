c1f092254c0e0e14f96178d7826edf6781e9b79b
Origin-zhao <enginezy@gmail.com>
2017-03-03 16:40:00 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
-echo 511 > /proc/sys/net/core/somaxconn
+#!/bin/sh
+echo 512 > /proc/sys/net/core/somaxconn
 echo "vm.overcommit_memory=1" > /etc/sysctl.conf
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
