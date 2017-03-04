2efb5c380af63cda7fc461e985c8ae4a71871798
Origin-zhao <enginezy@gmail.com>
2017-03-04 16:47:21 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo 511 > /proc/sys/net/core/somaxconn
-echo "vm.overcommit_memory=1" > /etc/sysctl.conf
+echo "vm.overcommit_memory = 1" > /etc/sysctl.conf
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
+sysctl -p
 
 set -e
 
